74633fe26efebb119dc3e8fa8ad511cd628be75b
Joe Smith <yasumoto7@gmail.com>
2018-02-20 19:20:11 -0800

    Latest CTLS

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Menkyo"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/ctls.git", from: "1.1.0")
+        .package(url: "https://github.com/vapor/ctls.git", from: "1.1.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
